9c5104fe0dd17f8c3fe21c10e6842b4eb1e730d8
mokaweb <info@moka.ch>
2014-12-22 22:11:44 +0100

    initial project version

--- classes/Countdown.php
+++ classes/Countdown.php
@@ -18,18 +18,18 @@ class Countdown extends Frontend {
 			// Splitte in Teilelemente auf (Trennzeichen ist ein Bindestrich)
 			$countdown = explode('-', $value[1]);
 			
-			//Gib den Endzeitpunkt an
-			//$endTime = mktime(0, 0, 0, 12, 18, 2014); //Stunde, Minute, Sekunde, Monat, Tag, Jahr; 
+			// Gib den Endzeitpunkt an
+			// $endTime = mktime(0, 0, 0, 12, 18, 2014); //Stunde, Minute, Sekunde, Monat, Tag, Jahr; 
 			
 			$endTime = mktime($countdown[0], $countdown[1], $countdown[2], $countdown[3], $countdown[4], $countdown[5]); //Stunde, Minute, Sekunde, Monat, Tag, Jahr; 
 			
-			//Gib die aktuelle Zeit des Microtimestamps von PHP5 aus
+			// Gib die aktuelle Zeit des Microtimestamps von PHP5 aus
 			$timeNow = microtime(true); 
 			
-			//Berechne die Differenz von der Endzeit bis zum jetzigen Zeitpunkt 
+			// Berechne die Differenz von der Endzeit bis zum jetzigen Zeitpunkt 
 			$diffTime = $endTime - $timeNow; 
 			
-			//Berechne die Tage, Stunden, Minuten und Sekunden
+			// Berechne die Tage, Stunden, Minuten und Sekunden
 			$day = floor($diffTime / (24*3600)); 
 			$diffTime = $diffTime % (24*3600); 
 			$hour = floor($diffTime / (60*60)); 
@@ -37,7 +37,7 @@ class Countdown extends Frontend {
 			$min = floor($diffTime / 60); 
 			$sec = $diffTime % 60; 
 			
-			//Formatiere die Einheiten
+			// Formatiere die Einheiten
 			
 			// Tage
 			if ($day == 1 || $day == -1) {
@@ -71,34 +71,34 @@ class Countdown extends Frontend {
 				$sec = " $sec Sekunden";
 			}
 			
-			//  Verberge die Tage, wenn sie abgelaufen sind
+			// Verberge die Tage, wenn sie abgelaufen sind
 			if ($day <= 0) {
 				$day = "";
 			}
 			
-			// verberge die Stunden, wenn sie abgelaufen sind
+			// Verberge die Stunden, wenn sie abgelaufen sind
 			if ($day <= 0 && $hour <= 0) {
 				$hour = "";
 			}
 			
-			// verberge die Minuten, wenn sie abgelaufen sind
+			// Verberge die Minuten, wenn sie abgelaufen sind
 			if ($day <= 0 && $hour <= 0 && $min <= 0) {
 				$min = "";
 			}
 			
-			// verberge die Sekunden, wenn sie abgelaufen sind und gib eine Meldung aus
+			// Verberge die Sekunden, wenn sie abgelaufen sind und gib eine Meldung aus
 			if ($day <= 0 && $hour <= 0 && $min <= 0 && $sec <= 0) {
 				$sec = "(oh - der Termin ist abgelaufen)";
 			}
 
-			// zeige Std. Min. Sek. nur am letzten Tag
+			// Zeige Std. Min. Sek. nur am letzten Tag
 			if ($day > 1) {
 				$hour = "";
 				$min = "";
 				$sec = "";
 			}
 			
-			//Gib die Einheiten aus: $day (Tage), $hour (Stunden), $sec (Sekunden)
+			// Gib die Einheiten aus: $day (Tage), $hour (Stunden), $sec (Sekunden)
 			$content  = $day; 
 			$content .= $hour; 
 			$content .= $min;
@@ -112,4 +112,3 @@ class Countdown extends Frontend {
 
 }
 
-	
