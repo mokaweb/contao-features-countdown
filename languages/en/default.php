f14a0ae8861282ea4b0a56f1c970571caed00dbf
mokaweb <info@moka.ch>
2014-12-23 18:23:34 +0100

    englische Übersetzung hinzugefügt

---
+++ languages/en/default.php
@@ -0,0 +1,21 @@
+<?php
+
+$strLangDays = $GLOBALS['TL_LANG']['MSC']['countdown_simple']['days'] = 'days';
+
+$strLangDay = $GLOBALS['TL_LANG']['MSC']['countdown_simple']['day'] = 'day';
+
+$strLangHours = $GLOBALS['TL_LANG']['MSC']['countdown_simple']['hours'] = 'hours';
+
+$strLangHour = $GLOBALS['TL_LANG']['MSC']['countdown_simple']['hour'] = 'hour';
+
+$strLangMinutes = $GLOBALS['TL_LANG']['MSC']['countdown_simple']['minutes'] = 'minutes';
+
+$strLangMinute = $GLOBALS['TL_LANG']['MSC']['countdown_simple']['minute'] = 'minute';
+
+$strLangSeconds = $GLOBALS['TL_LANG']['MSC']['countdown_simple']['seconds'] = 'seconds';
+
+$strLangSecond = $GLOBALS['TL_LANG']['MSC']['countdown_simple']['second'] = 'second';
+
+$strLangExpired = $GLOBALS['TL_LANG']['MSC']['countdown_simple']['expired'] = '(ups, the date is expired)';
+
+?>
